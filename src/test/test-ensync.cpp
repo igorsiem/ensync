8c37b25360a6a778ab3e72a84d6ff17753dcea91
Igor Siemienowicz <igor@qprise.com>
2017-08-08 12:04:37 +0800

    Implemented simple build and test framework

---
+++ src/test/test-ensync.cpp
@@ -0,0 +1,10 @@
+/*
+ * This file is part of the enSync project, written by Igor Siemienowicz.
+ *
+ * Copyright (c) 2017 Igor Siemienowicz All rights reserved
+ * Distributed under the Boost Software License, Version 1.0 (see
+ * http://www.boost.org/LICENSE_1_0.txt).
+ */
+
+#define CATCH_CONFIG_MAIN
+#include <CATCH/catch.hpp>
