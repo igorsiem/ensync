16a98f10d7b04b9936c48136986c8f04629f3622
Igor Siemienowicz <igor@qprise.com>
2017-08-10 07:30:54 +0800

    (intermediate) Some notes on error handling
    Just checking this in before creating another branch for the string message table

---
+++ src/ensync/error.h
@@ -0,0 +1,15 @@
+/*
+ * This file is part of the enSync project, written by Igor Siemienowicz.
+ *
+ * Copyright (c) 2017 Igor Siemienowicz All rights reserved
+ * Distributed under the Boost Software License, Version 1.0 (see
+ * http://www.boost.org/LICENSE_1_0.txt).
+ */
+
+#ifndef enSync_error_h_included
+#define enSync_error_h_included
+
+namespace sync {
+}   // end sync namespace
+
+#endif
